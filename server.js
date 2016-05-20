2f91228bcc65e48a058d5247cc088d05d5191f5d
John Stallo <john.stallo@live.com>
2016-05-20 12:31:35 -0700

    demo reset

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.get('/api', function (req, res) {
 //     });
 // });
 
-
 // application -------------------------------------------------------------
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');     // load the single view file (angular will handle the page changes on the front-end)
