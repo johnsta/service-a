ca14e6a2c59bff99abbc1bfca02bd690b44cc815
John Stallo <john.stallo@live.com>
2016-09-27 11:13:09 -0700

    service-a in single repo

--- server.js
+++ server.js
@@ -1,45 +1,50 @@
 var os = require('os');
 var request = require('request');
 var morgan = require('morgan');
-
+if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    var appInsights = require('applicationinsights').setup().start();
+    appInsights.client.commonProperties = {
+        "Service name": require("./package.json").name
+    };
+}
 var express = require('express');
-var app = express();
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(require("morgan")("dev"));
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(morgan("dev"));
 
 // api ------------------------------------------------------------
 app.get('/api', function (req, res) {
-    res.send('Hello from service A running on ' + os.hostname());
-
     // Connect to redis container using environment variable
-    // var redis = require('redis').createClient(process.env.MYREDIS_PORT, process.env.MYREDIS_DOMAIN);
+    // var redis = require('redis').createClient("redis://myredis");
 
     // Increment requestCount each time API is called
     // redis.incr('requestCount', function (err, reply) {
     //     var requestCount = reply;
     // });
 
-    // Invoke service-go
-    // request(process.env.SERVICE_GO_URL, function (error, response, body) {
-    //     res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
-    // });
-});
-
-app.get('/metrics', function (req, res) {
-    var redis = require('redis').createClient(process.env.MYREDIS_PORT, process.env.MYREDIS_DOMAIN);
-    redis.get('requestCount', function (err, reply) {
-        res.send({ requestCount: reply });
+    // Invoke service-b
+    request('http://service-b', function (error, response, body) {
+         res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
     });
 });
 
 // application -------------------------------------------------------------
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');     // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-var port = process.env.PORT || 4000;
+// app.get('/metrics', function (req, res) {
+// var redis = require('redis').createClient("redis://myredis");
+//     redis.get('requestCount', function (err, reply) {
+//         res.send({ requestCount: reply });
+//     });
+// });
+
+var port = 80;
 app.listen(port, function () {
-    console.log("Listening on port " + port);
+    console.log('Listening on port ' + port);
 });
+
+
 
